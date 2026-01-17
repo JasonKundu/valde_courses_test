9c511d84bf7fa3fb787d1e20be40dc8f4ac283c3
JasonKundu <128323797+JasonKundu@users.noreply.github.com>
2026-01-17 04:15:37 +0000

    commiting testing and whatnot

---
+++ Course_1/Printing.py
@@ -0,0 +1,6 @@
+# Example 1: Printing to the screen
+print("Hello, World!")
+print("My name is Python")
+
+
+
---
+++ Course_1/Variables.py
@@ -0,0 +1,15 @@
+# variables are used to store data
+
+# This is a string (show)
+name = "jason"
+print(name) # This is how i will see what the program is doing
+
+# You can add a string to another to make another string
+full_name = "jason" + "kundu"
+print(full_name) 
+# * notice how python uses exactly what you give it meaning it won't give you a space. it isn't smart ;) !
+
+name = "Alex"
+age = 15
+favorite_number = 7.9
+
