a26af18d2597a1f88606b5df2b441e72f7ce578d
JasonKundu <128323797+JasonKundu@users.noreply.github.com>
2026-01-17 04:16:53 +0000

    werh

--- Course_1/Variables.py
+++ Course_1/Variables.py
@@ -7,8 +7,8 @@ print(name) # This is how i will see what the program is doing
 # You can add a string to another to make another string
 full_name = "jason" + "kundu"
 print(full_name) 
-# * notice how python uses exactly what you give it meaning it won't give you a space. it isn't smart ;) !
 
+# * Notice how python uses exactly what you give it meaning it won't give you a space. it isn't that smart :( !
 name = "Alex"
 age = 15
 favorite_number = 7.9
